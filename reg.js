3be1ec6c025d75f3bc05ed110b39c074cbf8731c
BonningtonDublin <147234368+BonningtonDublin@users.noreply.github.com>
2024-05-31 07:47:16 +0100

    2

---
+++ reg.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  $("#GUEST").click(function(){
+    $("#GUEST-INFO").show();
+    document.getElementById('OBSERVATIONS').value = 'Hotel Guest';
+  });
+  $("#CROFT").click(function(){
+    $("#GUEST-INFO").hide();
+    document.getElementById('ROOM').required = false;
+    document.getElementById('CHECK-OUT').value = today;
+    document.getElementById('OBSERVATIONS').value = 'Croft Bar';
+  });
+  $("#MCG").click(function(){
+    $("#GUEST-INFO").hide();
+    document.getElementById('ROOM').required = false;
+    document.getElementById('CHECK-OUT').value = today;
+    document.getElementById('OBSERVATIONS').value = "McGettigan's";
+  });
+});
+
+// getFullYear, getMonth, getDate, getHours, getMinutes all return values of local time.
+const convertToDateTimeLocalString = (date) => {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+
+  return `${year}-${month}-${day}T${hours}:${minutes}`;
+}
+
+//Check-in date as today
+const currentTime = new Date();
+const today = convertToDateTimeLocalString(currentTime);
+document.getElementById('CHECK-IN').value = today;
+
+//Minimun check-out date from 1 day ahead
+var minDate = new Date(currentTime.setDate(currentTime.getDate() + 1)).toISOString().split("T")[0];
+document.getElementsByName("CHECK-OUT")[0].setAttribute('min', minDate);
